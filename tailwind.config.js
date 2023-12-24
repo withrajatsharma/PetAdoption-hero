dd4686e2de63795a426ac882f7a86f0025e4de06
RajatSharma <withrajatsharma@gmail.com>
2023-12-24 18:25:33 +0530

    nav completed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      textColor:{
+        purpletext:"rgba(129, 135, 220, 1)",
+      }
+    },
+  },
+  plugins: [],
+}
+
